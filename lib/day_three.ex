7183ed0f31eb14c16fcb5ba169b689d47ce22322
Travis Roberts <traviskroberts@gmail.com>
2021-12-03 18:26:29 -0600

    Add solution for day three (first problem)

---
+++ lib/day_three.ex
@@ -0,0 +1,42 @@
+defmodule AdventOfCode.DayThree do
+  def process(data) do
+    data
+    |> Enum.map(&parse/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&get_occurences/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&convert_to_binary/1)
+    |> multiply()
+  end
+
+  defp convert_to_binary(list) do
+    {value, _rem} =
+      list
+      |> Enum.join()
+      |> Integer.parse(2)
+
+    value
+  end
+
+  defp get_occurences(list) do
+    zeros = Enum.count(list, fn item -> item == 0 end)
+    ones = Enum.count(list, fn item -> item == 1 end)
+
+    if zeros > ones, do: [0, 1], else: [1, 0]
+  end
+
+  defp multiply([head | []]), do: head
+  defp multiply([head | tail]) do
+    head * multiply(tail)
+  end
+
+  defp parse(item) do
+    item
+    |> Integer.to_string()
+    |> String.pad_leading(12, "0")
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+  end
+end
